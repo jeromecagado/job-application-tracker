3727d0153876a08b530587785a6a449d99a2d224
Jerome <jerome.cagado@outlook.com>
2025-07-11 15:28:17 -0700

    Added starter code with interface to JobApplicationRepository.

--- src/main/java/com/jerome/jobtracker/repository/JobApplicationRepository.java
+++ src/main/java/com/jerome/jobtracker/repository/JobApplicationRepository.java
@@ -1,4 +1,10 @@
 package com.jerome.jobtracker.repository;
 
-public class JobApplicationRepository {
+import com.jerome.jobtracker.model.JobApplication;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JobApplicationRepository extends JpaRepository<JobApplication, Long> {
+    // You can add custom query methods later (e.g., findByStatus)
 }
